c21f5c48bd698d17c42281b42d20c9ff3f8f941d
Alexander Powell <ajpowell@email.wm.edu>
2019-08-08 16:39:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mic-admin-scripts
---
+++ scripts.py
@@ -0,0 +1,14 @@
+# 
+# 
+# 
+# 
+# 
+
+import config
+
+def delete_records_older_than_one_month():
+	pass
+
+if __name__ == "__main__":
+
+	delete_records_older_than_one_month()

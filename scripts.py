36822d215b5191090aa150f41c076427949184da
Alexander Powell <ajpowell@email.wm.edu>
2019-08-23 08:37:39 -0400

    new functions added

--- scripts.py
+++ scripts.py
@@ -7,9 +7,8 @@
 from datetime import datetime, timedelta
 import firebase_admin
 from firebase_admin import credentials, firestore
+from firebase_admin import auth
 import config
-
-UID = 'xgdRnVu3yrgjEhrMQgDSImBEOCc2'
 
 cred = credentials.Certificate(config.serviceAccountKey)
 firebase_admin.initialize_app(cred)
@@ -28,9 +27,68 @@ def delete_records_older_than_one_month():
 		deleted = deleted + 1
 	print(str(deleted) + ' records deleted.')
 
+def clear_upload_file_data(uid, batch_size):
+	coll_ref = db.collection('formUploads/' + uid + '/uploadFormData')
+	docs = coll_ref.limit(batch_size).stream()
+	deleted = 0
+
+	for doc in docs:
+		print('ID: ' + doc.id)
+		doc.reference.delete()
+		deleted = deleted + 1
+
+	if deleted >= batch_size:
+		return clear_upload_file_data(uid, batch_size)
+
 def clear_deleted_user_data():
 	pass
 
+def list_users():
+	page = auth.list_users()
+	while page:
+		for user in page.users:
+			print('UID: {0}, Name: {1}, Email: {2}, Email verified: {3}'.format(user.uid, user.display_name, user.email, user.email_verified))
+		page = page.get_next_page()
+
+def list_premium_users():
+	pass
+
+def revoke_user_token(uid):
+	auth.revoke_refresh_tokens(uid)
+	user = auth.get_user(uid)
+
+def revoke_all_user_tokens():
+	page = auth.list_users()
+	while page:
+		for user in page.users:
+			auth.revoke_refresh_tokens(user.uid)
+			user = auth.get_user(user.uid)
+		page = page.get_next_page()
+
 if __name__ == "__main__":
 
-	delete_records_older_than_one_month()
+	#delete_records_older_than_one_month()
+
+	'''query = db.collection('formUploads/<UID>/uploadFormData')
+
+	docs = query.stream()
+	for doc in docs:
+		query.document(doc.id).update({'timestamp' : datetime.now() + timedelta(hours=4), 'isCompleted' : False})
+		#print(doc.to_dict())'''
+
+	user = auth.get_user_by_email('lotrrox@gmail.com')
+	print(user.displayName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
